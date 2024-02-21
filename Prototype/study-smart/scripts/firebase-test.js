efd06bb15df326f6a09bf8121cd2ca198de93514
raziel <92149572+tiny-raziel@users.noreply.github.com>
2024-02-21 19:39:08 +0100

    Massive refactor. Everything is async now and you load stuff from Firebase instead of the JSON

--- Prototype/study-smart/scripts/firebase-test.js
+++ Prototype/study-smart/scripts/firebase-test.js
@@ -1,15 +1,72 @@
 import { db } from "../firebase/firebase-setup.js";
 import { collection, getDocs } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 
-// Async function to get documents from a collection
 async function getMyCollection() {
-    console.log("yay");
     var toGet = "section" + "01"; //so we can cycle through
     const querySnapshot = await getDocs(collection(db, toGet));
     querySnapshot.forEach((doc) => {
       console.log(doc.id, " => ", doc.data());
     });
+    const items = querySnapshot.docs.map(x => ({id: x.id, data: x.data()}))
+    const json = JSON.stringify(items, null, 2);
+    const jsonArray = JSON.parse(json)
+    console.log(jsonArray.find(item => item.id === "content").data["1"]);
+    console.log(jsonArray.find(item => item.id === "header").data["1"]);
+    console.log(jsonArray.find(item => item.id === "choices").data["0"].text);
+    console.log(jsonArray.find(item => item.id === "choices").data["0"].jump);
 }
 
-// Call the function
-getMyCollection();
+export async function getContent(sectionID) {
+  try {
+    const querySnapshot = await getDocs(collection(db, sectionID));
+    const items = querySnapshot.docs.map(x => ({ id: x.id, data: x.data() }));
+    const json = JSON.stringify(items, null, 2);
+    const jsonArray = JSON.parse(json);
+    const contentData = jsonArray.find(item => item.id === "content").data["1"];
+    return contentData;
+  } catch (error) {
+    console.error("Error fetching content:", error);
+    throw error; // Re-throw the error to be caught by the calling function
+  }
+}
+
+export async function getHeader(sectionID) {
+  try {
+    const querySnapshot = await getDocs(collection(db, sectionID));
+    const items = querySnapshot.docs.map(x => ({id: x.id, data: x.data()}))
+    const json = JSON.stringify(items, null, 2);
+    const jsonArray = JSON.parse(json)
+    const header = jsonArray.find(item => item.id === "header");
+    if (header != null) {
+      return header.data["1"];
+    } else {
+      return null
+    }
+  } catch (error) {
+    console.error("Error fetching header:", error);
+    throw error; // Re-throw the error to be caught by the calling function
+  }
+}
+
+export async function getChoices(sectionID) {
+  const querySnapshot = await getDocs(collection(db, sectionID));
+  const items = querySnapshot.docs.map(x => ({id: x.id, data: x.data()}))
+  const json = JSON.stringify(items, null, 2);
+  const jsonArray = JSON.parse(json)
+  const choices = jsonArray.find(item => item.id === "choices");
+  if (choices && choices.data) {
+    let choiceArray = [];
+    const keys = Object.keys(choices.data);
+    keys.forEach(key => {
+      const element = choices.data[key];
+      choiceArray.push([element.jump, element.text]);
+    });
+    return choiceArray;
+  }
+}
+
+//getMyCollection();
+
+
+
+
--- Prototype/study-smart/scripts/sections-navigation.js
+++ Prototype/study-smart/scripts/sections-navigation.js
@@ -2,39 +2,73 @@
 //LOAD LATEST SAVE DATA OR START FROM THE BEGINNING
 //AT END OF SECTION (WHEN CONT BUTTON PRESSED) SAVE SECTION AS COMPLETED USING SAVE AND LOAD
 
+import { getContent, getHeader, getChoices } from "../scripts/firebase-test.js";
+
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("Dom loaded");
     var jsonData = {}; // initialise json
 
-    //Loads new section to update content
-    function updateContent(sectionId) {
-        console.log('Lade Sektion:', sectionId);
-        var data = jsonData[sectionId];
-        if (!data) {
-            console.error('Sektion nicht gefunden:', sectionId);
-            return;
-        }
-
-        //Only Update the section header and image if new data is in the section
-        if (data.header) {
-            document.getElementById("section-header").textContent = data.header
-        }
-
-        //placeholder until all sections get content!!
-        document.getElementById("section-content").innerHTML = data.content || "<p>Standardinhalt</p>";
-
-        //Delete all previous buttons
+    function initSection() {
+        //LATER: call loadSaveData() and work with that for the first section
         var continueDiv = document.getElementById("section-continue");
         continueDiv.innerHTML = '';
+        updateHeader("section01");
+        updateContent("section01");
+        updateChoices("section10", continueDiv);
+    }
+    
+    // Lädt JSON-Daten und setzt den initialen Inhalt
+    fetch('../content/sections.json')
+        .then(response => response.json())
+        .then(data => {
+            jsonData = data; // Speichert die geladenen JSON-Daten in `jsonData`
+            initSection(); // Startet mit der ersten Sektion
+        })
+        .catch(error => console.error('Error loading JSON data:', error));
+}, false);
 
-        //Create between 1-4 Buttons, depending on the choices
-        //if there is only one button, no custom text will be used
-        if (data.choices.length > 1) {
-            data.choices.forEach(choice => {
+export function updateSection(sectionId) {
+    console.log('Lade Sektion:', sectionId);
+    //remove the old buttons
+    updateHeader(sectionId);
+    updateContent(sectionId);
+    updateChoices(sectionId);
+}
+
+
+async function updateContent(sectionId) {
+    try {
+        const content = await getContent(sectionId);
+        document.getElementById("section-content").innerHTML = content || "<p>Standardinhalt</p>";
+    } catch (error) {
+        console.error("Error fetching content:", error);
+    }
+}
+
+async function updateHeader(sectionId) {
+    try {
+        const header = await getHeader(sectionId);
+        if(header) {
+            document.getElementById("section-header").innerHTML = header;
+        }
+    } catch (error) {
+        console.error("Error fetching header:", error);
+    }
+}
+
+async function updateChoices(sectionId) {
+    //Create between 1-4 Buttons, depending on the choices
+    //if there is only one button, no custom text will be used
+    try {
+        var continueDiv = document.getElementById("section-continue");
+        continueDiv.innerHTML = '';
+        const choices = await getChoices(sectionId);
+        console.log(choices.length);
+        if (choices.length > 1) {
+            choices.forEach(choice => {
                 var button = document.createElement("button");
-                button.textContent = choice.text;
+                button.textContent = choice[1];
                 button.classList.add("section-continue-custom");
-                button.setAttribute('data-section', choice.jump);
+                button.setAttribute('data-section', choice[0]);
                 button.addEventListener('click', function() {
                     updateContent(this.getAttribute('data-section'));
                 });
@@ -44,20 +78,13 @@ document.addEventListener('DOMContentLoaded', function() {
             var button = document.createElement("button");
             button.textContent = "";
             button.classList.add("section-continue-default");
-            button.setAttribute('data-section', data.choices[0].jump);
-            button.addEventListener('click', function() {
-                updateContent(this.getAttribute('data-section'));
+            button.setAttribute('data-section', choices[0][0]);
+            button.addEventListener('click', () => {
+                updateSection(button.getAttribute('data-section'));
             });
             continueDiv.appendChild(button);
           }
+    } catch (error) {
+        console.error("Error fetching choices:", error);
     }
-
-    // Lädt JSON-Daten und setzt den initialen Inhalt
-    fetch('../content/sections.json')
-        .then(response => response.json())
-        .then(data => {
-            jsonData = data; // Speichert die geladenen JSON-Daten in `jsonData`
-            updateContent("section01"); // Startet mit der ersten Sektion
-        })
-        .catch(error => console.error('Error loading JSON data:', error));
-}, false);
+}
