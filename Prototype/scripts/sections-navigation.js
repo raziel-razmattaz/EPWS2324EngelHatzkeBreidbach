0dceee69bc78e1712c8ad097f10823f7976c1901
raziel <116152143+raziel-razmattaz@users.noreply.github.com>
2024-02-21 10:19:27 +0100

    Revert "Ines rest"

--- Prototype/scripts/sections-navigation.js
+++ Prototype/scripts/sections-navigation.js
@@ -1,30 +1,9 @@
-import * as saveAndLoad from "./save-and-load.js"
-import { } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js'
-import * as firebase from "../firebase/firebase.js"
-
-/*
-savesection = addsavepoint -> in localstorage mit id von firebase
-update content: infos von firebase laden
-initialize content: recent savepoint von localstorage mit id(default section01)
-                            -> abruf content von firebase
- */
-
-document.addEventListener("DOMContentLoaded", function () {
-    if (!saveAndLoad.checkLocalStorage()) {
-        return alert('localStorage ist in Ihrem Browser nicht verfügbar oder aufgrund von Sicherheitseinstellungen nicht nutzbar. Bitte überprüfen Sie Ihre Browser-Einstellungen.');
-    }
-    let jsonData = {}; // initialise json
-
-    // Function to be called every time a new section is entered
-    function saveSection(sectionId) {
-        // Add a new save point with the current timestamp
-        saveAndLoad.addSavePoint(sectionId);
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    var jsonData = {}; // initialise json
 
     //Loads new section to update content
     function updateContent(sectionId) {
         console.log('Lade Sektion:', sectionId);
-        saveSection(sectionId); // Set a save point whenever a new section is loaded
         var data = jsonData[sectionId];
         if (!data) {
             console.error('Sektion nicht gefunden:', sectionId);
@@ -51,65 +30,29 @@ document.addEventListener("DOMContentLoaded", function () {
                 button.textContent = choice.text;
                 button.classList.add("section-continue-custom");
                 button.setAttribute('data-section', choice.jump);
-                button.addEventListener('click', function () {
+                button.addEventListener('click', function() {
                     updateContent(this.getAttribute('data-section'));
                 });
                 continueDiv.appendChild(button);
             });
-        } else {
+          } else {
             var button = document.createElement("button");
             button.textContent = "";
             button.classList.add("section-continue-default");
             button.setAttribute('data-section', data.choices[0].jump);
-            button.addEventListener('click', function () {
+            button.addEventListener('click', function() {
                 updateContent(this.getAttribute('data-section'));
             });
             continueDiv.appendChild(button);
-        }
-    }
-
-    // Function to initialize the content with the most recent save point or default section
-    function initializeContent() {
-        var mostRecentSection = saveAndLoad.getMostRecentSavePoint() || "section01";
-        updateContent(mostRecentSection);
-    }
-
-    async function getSection(db){
-        const section = collection(db, 'sec')
+          }
     }
 
     // Lädt JSON-Daten und setzt den initialen Inhalt
-    fetch('https://entwicklungsprojekt-ed4aa.firebaseio.com')
+    fetch('../content/sections.json')
         .then(response => response.json())
         .then(data => {
             jsonData = data; // Speichert die geladenen JSON-Daten in `jsonData`
-            console.log(jsonData)
-            initializeContent(); // Initialize with the most recent save point or the first section
+            updateContent("section01"); // Startet mit der ersten Sektion
         })
         .catch(error => console.error('Error loading JSON data:', error));
 });
-
-// Get a list of cities from your database
-async function getCities(db) {
-    const citiesCol = collection(db, 'cities');
-    const citySnapshot = await getDocs(citiesCol);
-    const cityList = citySnapshot.docs.map(doc => doc.data());
-    return cityList;
-
-/*
-suspend fun checkNonEdibility(product: Product): Boolean {
-    return getUserPreferences(getUser()!!).any{
-        val preference = preferencesList[it.preferenceName]
-        preference!!.third.invoke(product)
-    }
-}
-
-suspend fun getUserPreferences(email: String): List<Preference> {
-    return db.collection(PREFERENCES_COLLECTION).whereEqualTo("userEmail",email).get().await().toObjects()
-}
-*/
-
-
-
-
-
