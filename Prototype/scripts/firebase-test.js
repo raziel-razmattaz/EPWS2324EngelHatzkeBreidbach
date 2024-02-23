0971ee8d377a2661258a39528e174826267135b2
raziel <116152143+raziel-razmattaz@users.noreply.github.com>
2024-02-23 12:43:02 +0100

    Delete Prototype/scripts directory

--- Prototype/scripts/firebase-test.js
+++
@@ -1,5 +0,0 @@
-db.collection("meineSammlung").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
-    });
-  });
--- Prototype/scripts/ui.js
+++
@@ -1,9 +0,0 @@
-//opening and closing the overlay menu
-
-function openMenuOverlay() {
-    document.getElementById("overlay-menu").style.width = "100%";
-  }
-
-  function closeMenuOverlay() {
-    document.getElementById("overlay-menu").style.width = "0%";
-  } 
